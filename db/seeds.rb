43398bc0ef9e7c94f941c797a39622b372dc5328
Egor Pozharov <chedius232@gmail.com>
2021-11-02 10:53:53 -0700

    fixed seed algo

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-authors = Author.create([{name: 'a1'}, {name: 'a2'}, {name: 'a3'}, {name: 'a4'}, {name: 'a5'}, {name: 'a6'}, {name: 'a7'}, {name: 'a8'}, {name: 'a9'}, {name: 'a10'}])
-books = Book.create([{name: 'b1', author_id: 1}, {name: 'b2', author_id: 1}, {name: 'b3', author_id: 1}, {name: 'b4', author_id: 1}, {name: 'b5', author_id: 1}, {name: 'b6', author_id: 1}, {name: 'b7', author_id: 1}, {name: 'b8', author_id: 1}, {name: 'b9', author_id: 1}, {name: 'b10', author_id: 1},
-{name: 'b1', author_id: 2}, {name: 'b2', author_id: 2}, {name: 'b3', author_id: 2}, {name: 'b4', author_id: 2}, {name: 'b5', author_id: 2}, {name: 'b6', author_id: 2}, {name: 'b7', author_id: 2}, {name: 'b8', author_id: 2}, {name: 'b9', author_id: 2}, {name: 'b10', author_id: 2},
-{name: 'b1', author_id: 3}, {name: 'b2', author_id: 3}, {name: 'b3', author_id: 3}, {name: 'b4', author_id: 3}, {name: 'b5', author_id: 3}, {name: 'b6', author_id: 3}, {name: 'b7', author_id: 3}, {name: 'b8', author_id: 3}, {name: 'b9', author_id: 3}, {name: 'b10', author_id: 3},
-{name: 'b1', author_id: 4}, {name: 'b2', author_id: 4}, {name: 'b3', author_id: 4}, {name: 'b4', author_id: 4}, {name: 'b5', author_id: 4}, {name: 'b6', author_id: 4}, {name: 'b7', author_id: 4}, {name: 'b8', author_id: 4}, {name: 'b9', author_id: 4}, {name: 'b10', author_id: 4},
-{name: 'b1', author_id: 5}, {name: 'b2', author_id: 5}, {name: 'b3', author_id: 5}, {name: 'b4', author_id: 5}, {name: 'b5', author_id: 5}, {name: 'b6', author_id: 5}, {name: 'b7', author_id: 5}, {name: 'b8', author_id: 5}, {name: 'b9', author_id: 5}, {name: 'b10', author_id: 5},
-{name: 'b1', author_id: 6}, {name: 'b2', author_id: 6}, {name: 'b3', author_id: 6}, {name: 'b4', author_id: 6}, {name: 'b5', author_id: 6}, {name: 'b6', author_id: 6}, {name: 'b7', author_id: 6}, {name: 'b8', author_id: 6}, {name: 'b9', author_id: 6}, {name: 'b10', author_id: 6},
-{name: 'b1', author_id: 7}, {name: 'b2', author_id: 7}, {name: 'b3', author_id: 7}, {name: 'b4', author_id: 7}, {name: 'b5', author_id: 7}, {name: 'b6', author_id: 7}, {name: 'b7', author_id: 7}, {name: 'b8', author_id: 7}, {name: 'b9', author_id: 7}, {name: 'b10', author_id: 7},
-{name: 'b1', author_id: 8}, {name: 'b2', author_id: 8}, {name: 'b3', author_id: 8}, {name: 'b4', author_id: 8}, {name: 'b5', author_id: 8}, {name: 'b6', author_id: 8}, {name: 'b7', author_id: 8}, {name: 'b8', author_id: 8}, {name: 'b9', author_id: 8}, {name: 'b10', author_id: 8},
-{name: 'b1', author_id: 9}, {name: 'b2', author_id: 9}, {name: 'b3', author_id: 9}, {name: 'b4', author_id: 9}, {name: 'b5', author_id: 9}, {name: 'b6', author_id: 9}, {name: 'b7', author_id: 9}, {name: 'b8', author_id: 9}, {name: 'b9', author_id: 9}, {name: 'b10', author_id: 9},
-{name: 'b1', author_id: 10}, {name: 'b2', author_id: 10}, {name: 'b3', author_id: 10}, {name: 'b4', author_id: 10}, {name: 'b5', author_id: 10}, {name: 'b6', author_id: 10}, {name: 'b7', author_id: 10}, {name: 'b8', author_id: 10}, {name: 'b9', author_id: 10}, {name: 'b10', author_id: 10},
-])
+10.times do |i|
+  authors = Author.create(name: "a#{i}")
+  10.times do |j|
+    books = Book.create(name: "b#{j}", author_id: "#{i}")
+end
+end
+
+
